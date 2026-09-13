a4d17d5eef5084525ea9f864297352cfb7b94719
Wei Siang Tan <63801664+tanws01@users.noreply.github.com>
2026-09-13 11:45:58 +0800

    fix: use exported Express app in Vercel function

--- api/index.js
+++ api/index.js
@@ -1,14 +1,3 @@
-import express from "express";
-
-// src/server.js is also used for local development and starts its own HTTP
-// listener. On Vercel, we only need the Express app as the serverless handler.
-const originalListen = express.application.listen;
-express.application.listen = function () {
-  return this;
-};
-
-const { default: app } = await import("../src/server.js");
-
-express.application.listen = originalListen;
+import { app } from "../src/server.js";
 
 export default app;
